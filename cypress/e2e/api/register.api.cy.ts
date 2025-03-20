335378cf1dd3a8a0fc62b06b67d308b90300b8e9
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-03-20 09:04:35 +0100

    ban08

--- cypress/e2e/api/register.api.cy.ts
+++ cypress/e2e/api/register.api.cy.ts
@@ -10,12 +10,26 @@ describe('Register API POST /users/signup tests', () => {
         const user = generateUser()
 
         // when + then
+        cy.register(user)
+    })
+
+    it('should return 400 if password too short', () => {
+        // given
+        const originalUser = generateUser()
+        const user = {
+            ...originalUser,
+            password: 'short'
+        }
+
+        // when + then
         cy.request({
             method: 'POST',
             url: 'http://localhost:4001/users/signup',
-            body: user
+            body: user,
+            failOnStatusCode: false
         }).then(response => {
-            expect(response.status).to.equal(201)
+            expect(response.status).to.equal(400)
+            expect(response.body.password).to.equal('Minimum password length: 8 characters')
         })
     })
 
